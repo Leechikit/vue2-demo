e96b90d2844e8f186753dd7795498f463beb1210
李子杰 <leechikit0823@163.com>
2017-06-29 22:00:56 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # vue2-demo
 
 一个使用Vue2.0 + Vue-router2.0 + Vuex2.0构建项目的demo
+
+| Name    | Description | Method |
+| ------- | ---------- | ----|
+| state   | 状态 | mapState |
+| getters | store的计算属性 | mapGetters |
+| mutations | store状态提交 | mapMutitions |
+| actions | 异步提交mutations | |
---
+++ src/components/header.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="header">
+		<div class="logo"></div>
+		<div class="title">{{title}}</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+  name: 'header',
+
+  data () {
+    return {
+
+    };
+  },
+  props:['title']
+};
+</script>
+
+<style lang="scss" scoped>
+.header{
+	display: flex;
+	height: 40px;
+}
+.logo{
+	flex-shrink:0;
+	width:40px;
+	height: 100%;
+	background:transparent url(../assets/logo.png) center center no-repeat;
+	background-size: 40px;
+}
+.title{
+	flex-grow:1;
+	font-size: 20px;
+	line-height: 40px;
+	height: 100%;
+}
+</style>
